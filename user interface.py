f7269d9910bdd66be380a783f55c6d142e0664f5
Sunil <146947097+SuniL-0007@users.noreply.github.com>
2025-01-24 22:07:30 +0530

    Added A User Interface python  code done using streamlit.

---
+++ user interface.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import joblib
+import streamlit as st
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+
+model = joblib.load("D:\sunil\Projects\ML project\ensemble_model_best.pkl")
+
+dataset = pd.read_csv("D:\sunil\Projects\ML project\Final_Data.csv")
+
+dataset = dataset.drop(columns=['price'])
+
+def predict_house_price(input_data):
+    input_df = pd.DataFrame([input_data])
+    input_df['total_rooms'] = input_df['bedRoom'] + input_df['bathroom'] + input_df['balcony']
+    input_df['house_age'] = input_df['agePossession']
+    input_df = input_df.drop(columns=['bedRoom', 'bathroom', 'balcony', 'agePossession'])
+    return input_df
+
+
+st.title("House Price Prediction")
+
+st.header("Enter House Data")
+user_input = {}
+for col in dataset.columns:
+    user_input[col] = st.text_input(f"Select {col}")
+    
+if st.button("Predict Price"):
+
+    final_input = predict_house_price(user_input)
+    try:
+
+        prediction = model.predict(final_input)
+        st.write(prediction[0])
+    except ValueError as e:
+        st.error(f"Prediction error: {e}")
